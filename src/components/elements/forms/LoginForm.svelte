b9c561e8120b361dd0c76bdd6ba5eae1f2cef437
Gustavo-Pinaculo <gustavo@pinaculodigital.com.br>
2025-04-11 08:48:10 -0300

    login

---
+++ src/components/elements/forms/LoginForm.svelte
@@ -0,0 +1,10 @@
+<script>
+	import MainInput from "$components/assets/inputs/MainInput.svelte";
+
+</script>
+
+<div class="flex flex-col items-center justify-center p-4">
+    <span>
+        <MainInput/>
+    </span>
+</div>
